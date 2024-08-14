a03d2642f6eb97a87dbf523cdaada02832291981
mokatildev <mokatildev@gmail.com>
2024-08-14 17:55:55 +0100

    Deployed Manager.sol

---
+++ ignition/modules/Manager.ts
@@ -0,0 +1,16 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import hre from "hardhat";
+
+const amount = hre.ethers.parseEther("1");
+
+const ManagerModule = buildModule("ManagerModule", (m) => {
+  const Amount = m.getParameter("amount", amount);
+
+  const manager = m.contract("Manager",[], {
+    value: Amount
+  });
+
+  return { manager };
+});
+
+export default ManagerModule;
