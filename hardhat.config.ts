ac7b9ab9d08b0e87e9ba0af39dc0699de0bf0781
mokatildev <mokatildev@gmail.com>
2024-08-14 17:58:52 +0100

    Update Hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,17 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1337, 
+    },
+  },
+  paths: {
+    artifacts: "./client/artifacts"
+  },
+  typechain: {
+    outDir: "./types",
+  }
 };
 
 export default config;
