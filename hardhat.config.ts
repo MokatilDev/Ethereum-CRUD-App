7d77da6653583c7436624e6c9700e4885fd931b1
mokatildev <mokatildev@gmail.com>
2024-08-16 18:12:29 +0100

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,18 +2,25 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.26",
   networks: {
-    hardhat: {
-      chainId: 1337, 
+    sepolia: {
+      url: "", // Replace your sepolia.infura.io url
+      accounts: [""] // Replace your wallet privect key
     },
+    hardhat: {
+      chainId: 1337
+    }
   },
   paths: {
     artifacts: "./client/artifacts"
   },
   typechain: {
     outDir: "./types",
-  }
+  },
+  etherscan: {
+    apiKey: "", // Replace you etherscan api key 
+  },
 };
 
 export default config;
